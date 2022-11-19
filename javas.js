8afa87565db94ccc0d7e6caed5432240e54a1dbd
Midman Scientific <marnuel19clerk@gmail.com>
2022-11-18 17:31:48 -0800

    First commit

---
+++ javas.js
@@ -0,0 +1,46 @@
+
+const tttt= document.getElementsByTagName("img")
+for(let img of tttt){
+    console.log(img.src);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+    
+
+
+
+
+        
+         
+
+      
+
+
+
+
+
